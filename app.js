097d6ff9984bd00e7fc07cd5dfcaa82f7f77c1ef
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2019-01-01 18:29:57 +0000

    Start talking to the API by getting the authenticated user display name shown

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
+var spotifyEndpoint = 'https://api.spotify.com/v1';
+
 new Vue({
     el: '#app',
     data: {
         spotifyClientId: 'f6ebcd26b8d948fe908c690e2cc718da',
-        fromAuthComplete: false,
-        fromAuthToken: '',
-        toAuthComplete: false,
-        toAuthToken: '',
+        fromAuthToken: null,
+        toAuthToken: null,
         albumCopyPercentage: 80,
         songCopyPercentage: 0,
-        progressLog: 'Waiting for authentication'
+        progressLog: 'Waiting for authentication\n',
+        fromAccName: null,
+        toAccName: null
     },
-    created: function() {
+    computed: {
+        fromAuthComplete: function () { return !!this.fromAuthToken; },
+        toAuthComplete: function () { return !!this.toAuthToken; },
+        fromAxios: function () {
+            if (!this.fromAuthComplete) return null;
+            var ai = axios.create({ baseURL: spotifyEndpoint });
+            ai.defaults.headers.common['Authorization'] = 'Bearer ' + this.fromAuthToken;
+            return ai;
+        },
+        toAxios: function () {
+            if (!this.toAuthComplete) return null;
+            var ai = axios.create({ baseURL: spotifyEndpoint });
+            ai.defaults.headers.common['Authorization'] = 'Bearer ' + this.toAuthToken;
+            return ai;
+        }
+    },
+    created: function () {
+        var vm = this;
         // get auth tokens from querystring
         var fragment = window.location.hash.substr(1);
         var params = queryStringToObject(fragment);
@@ -20,6 +39,7 @@ new Vue({
             } else if (params['state'] === 'authTo') {
                 document.cookie = 'authTo=' + encodeURIComponent(params['access_token']);
             } else console.log('This is strange...');
+            document.location.replace('#');
         }
         // check which authentications have been completed
         var cookies = document.cookie.split(';').reduce(function (acc, c) {
@@ -27,18 +47,27 @@ new Vue({
             acc[kv[0].trim()] = decodeURIComponent(kv[1]);
             return acc;
         }, {});
-        console.log(cookies);
+        // if auth is done get the account names (these could be computed
+        // properties using async-computed-properties plugin)
         if (cookies.hasOwnProperty('authFrom')) {
-            this.fromAuthComplete = true;
-            this.fromAuthToken = cookies['authFrom'];
+            vm.progressLog += 'Authenticated "from" account\n';
+            vm.fromAuthToken = cookies['authFrom'];
+            vm.fromAxios.get('/me')
+                .then(function (response) {
+                    vm.fromAccName = response.data.display_name;
+                }).catch(console.log.bind(console));
         }
         if (cookies.hasOwnProperty('authTo')) {
-            this.toAuthComplete = true;
-            this.toAuthToken = cookies['authTo'];
+            vm.progressLog += 'Authenticated "to" account\n';
+            vm.toAuthToken = cookies['authTo'];
+            vm.toAxios.get('/me')
+                .then(function (response) {
+                    vm.toAccName = response.data.display_name;
+                }).catch(console.log.bind(console));
         }
     },
     methods: {
-        authFrom: function() {
+        authFrom: function () {
             var qs = objectToQueryString({
                 client_id: this.spotifyClientId,
                 response_type: 'token',
@@ -48,7 +77,7 @@ new Vue({
             var newURI = 'https://accounts.spotify.com/authorize/?' + qs;
             window.location.replace(newURI);
         },
-        authTo: function() {
+        authTo: function () {
             var qs = objectToQueryString({
                 client_id: this.spotifyClientId,
                 response_type: 'token',
@@ -61,7 +90,7 @@ new Vue({
     }
 });
 
-function objectToQueryString(o) {
+function objectToQueryString (o) {
     var qs = Object
         .keys(o)
         .map(function (k) {
@@ -71,7 +100,7 @@ function objectToQueryString(o) {
     return qs;
 }
 
-function queryStringToObject(qs) {
+function queryStringToObject (qs) {
     return qs.split('&').reduce(function (acc, val) {
         var kv = val.split('=');
         acc[decodeURIComponent(kv[0])] = decodeURIComponent(kv[1]);
